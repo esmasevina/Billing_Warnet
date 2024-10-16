2e98552695874d165671a6087855d61756fa3ed5
esmasevina <esmanursevina78@gmail.com>
2024-10-16 16:44:05 +0700

    Program sederhana belajar java dengan program billing warnet

---
+++ src/program/sederhana/billing/warnet/ProgramSederhanaBillingWarnet.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program.sederhana.billing.warnet;
+
+/**
+ *
+ * @author Ardy Sendleep
+ */
+public class ProgramSederhanaBillingWarnet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame_billing_warnet Laund = new Frame_billing_warnet();
+        Laund.setVisible(true);
+    }
+}
